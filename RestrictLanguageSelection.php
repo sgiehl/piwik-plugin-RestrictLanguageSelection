adc169f2f0691147b46cf9323be3803b747757e4
sgiehl <stefan@piwik.org>
2016-11-01 22:08:55 +0100

    Initial plugin version for Piwik 3.x

---
+++ RestrictLanguageSelection.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Piwik - Open source web analytics
+ *
+ * @link http://piwik.org
+ * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+ */
+namespace Piwik\Plugins\RestrictLanguageSelection;
+
+use Piwik\Plugin;
+
+/**
+ *
+ */
+class RestrictLanguageSelection extends Plugin
+{
+    public static $disableRestrictions = false;
+
+    /**
+     * @see Piwik_Plugin::getListHooksRegistered
+     */
+    public function getListHooksRegistered()
+    {
+        return array(
+            'LanguageManager.getAvailableLanguages' => 'modifyAvailableLanguages',
+        );
+    }
+
+    public function modifyAvailableLanguages(&$languages)
+    {
+        if (self::$disableRestrictions) {
+            return;
+        }
+
+        $setting = new SystemSettings();
+        $availableLanguages = $setting->availableLanguages->getValue();
+        if (!empty($availableLanguages)) {
+            $languages = $availableLanguages;
+        }
+    }
+}
